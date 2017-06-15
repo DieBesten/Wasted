be3cacd6653454631107ebc6899d25748d65bfb3
PhilippKaltofen <pkaltofen@yahoo.com>
2017-06-15 12:04:37 +0200

    Prototyp Anfänge
    Grobes Layout ohne Funktionalität

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,9 @@
 var setup = function(){
 	createCanvas(windowWidth, windowHeight);
-	background(255);
+	background(255, 255, 255, 0);
 }
 
 var draw = function(){
-	background(255);
+	background(255, 255, 255, 0);
 }
+
